87dc2f033eb9af3ec3d863b66e2b396f2439189b
Sturgen <dodi.alexsander@gmail.com>
2021-05-18 05:29:15 +0700

    Init Project. Register User Endpoint

---
+++ user/entity.go
@@ -0,0 +1,17 @@
+package user
+
+import (
+	"time"
+)
+
+type User struct {
+	ID             int
+	Name           string
+	Occupation     string
+	Email          string
+	PasswordHash   string
+	AvatarFileName string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	Role           string
+}
